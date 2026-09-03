074f30d51faa8b09f1a64bb690cfdabad708dc7b
rafael <rafael.eslucas@gmail.com>
2026-09-03 17:56:43 -0300

    Changed database and removed wrong shadow in h2

--- javascript/db.js
+++ javascript/db.js
@@ -1,16 +1,16 @@
 import { createClient }
     from "https://esm.sh/@supabase/supabase-js";
-/*
-DB certo, mas não configurado ainda:
+
+// DB certo, mas não configurado ainda:
 const supabase = createClient(
     "https://hqiirmiglybihqcdylat.supabase.co/",
     "sb_publishable_vLfCeaRqlrTEx4jCp3NDmA_pR-bEkcS"
-);*/
+);
 
-// DB errado, mas semiconfigurado pra teste
+/* DB errado, mas semiconfigurado pra teste
 const supabase = createClient(
     "https://qhzdcvcwcgvasarnvonu.supabase.co",
     "sb_publishable_CdHK73P-jNYM4jhCeY6o6A_5FRkfFmQ"
-)
+);*/
 
 export default supabase;
