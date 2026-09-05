136fd2772e6e06f81dc64a71e948e862850e0f4d
rafael <rafael.eslucas@gmail.com>
2026-09-05 12:17:23 -0300

    Now you can write new topics, and sign in still has the problem with profiles

--- index.js
+++ index.js
@@ -42,3 +42,32 @@ for (const topic of data) {
     });
 }
 
+const topic_submit = document.querySelector("#topic-submit");
+
+topic_submit.addEventListener("click", async() => {
+    const textarea = document.querySelector("#start-topic");
+    const topictitle = document.querySelector("#start-topic-title");
+
+    const text = textarea.value;
+    const title = topictitle.value;
+
+    const { data: { user }, error: auth_error } = await supabase.auth.getUser();
+
+    if (auth_error) {
+        console.log(auth_error);
+        return;
+    }
+    if (!user) {
+        console.log("no user");
+        return;
+    }
+
+    const { data, error } = await supabase.from("topics").insert({
+        title: title,
+        content: text,
+        author_id: user.id,
+    }).select();
+
+    if (error) console.log(error);
+    console.log(data);
+});
--- signup.js
+++ signup.js
@@ -8,17 +8,30 @@ form.addEventListener('submit', async (e) => {
 
     const email = form.email.value;
     const password = form.password.value;
+    const username = form.username.value;
 
-    const {data, error} = await supabase.auth.signUp({
+    const {data: signup_data, error: auth_error} = await supabase.auth.signUp({
         email: email,
         password: password,
     });
 
-    if (error) {
+    if (auth_error) {
         pSuccess.innerText = "Erro!";
-        console.log(error);
-    } else {
-        pSuccess.innerHTML = "Sua conta foi criada com sucesso! <a href='/'>Voltar para página inicial</a>";
+        console.log(auth_error);
+        return;
     }
-    console.log(data);
+
+    const { data: profile_data, error: profile_error } = await supabase.from("profiles").insert({
+        username: username,
+        id: signup_data.user.id,
+    }).select();
+
+    if (profile_error) {
+        console.log(profile_error);
+        return;
+    }
+
+    pSuccess.innerHTML = "Sua conta foi criada com sucesso! <a href='/'>Voltar para página inicial</a>";
+
+    console.log(profile_data);
 });
