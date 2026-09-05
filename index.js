0676228c005d49367195481a8da57538f7ff1192
rafael <rafael.eslucas@gmail.com>
2026-09-05 16:30:48 -0300

    Now you can load more topics

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
-let forum = document.querySelector("#forum");
-
-// Ajuste de estilo
-let nav = document.querySelector("nav");
-forum.style.height = `${window.innerHeight - nav.offsetHeight}px`;
-
-
-
+import { loadPage } from "./js/func.js"
 import supabase from "./js/db.js";
 
-const { data } = await supabase.from("topics").select("*");
+let forum = document.querySelector("#forum");
+let loadmore = document.querySelector("#load-more");
 
-console.log(data);
+const pagesize = 10;
 
-for (const topic of data) {
-    const {title, content, author_id, id} = topic;
+loadPage(0, pagesize, forum);
+let pages = 1;
 
-    const div = document.createElement("div");
-    forum.appendChild(div);
-    div.classList.add("topico");
-
-    const topic_title = document.createElement("h1");
-    topic_title.textContent = title;
-    div.appendChild(topic_title);
-
-    const topic_text = document.createElement("p");
-    topic_text.textContent = content;
-    div.appendChild(topic_text);
-
-    const topic_author = document.createElement("p");
-    const {data: author} = await supabase.from("profiles").select("*").eq("id", author_id).single();
-    topic_author.textContent = "Written by: " + author.username;
-    topic_author.classList.add("text-right");
-    div.appendChild(topic_author);
-    /* Dentre outras */
-
-    console.log(id);
-    div.addEventListener("click", () => {
-        let url = `./topic?id=${id}`;
-        window.location.href = url;
-        console.log(url);
-    });
-}
+loadmore.addEventListener("click", () => {
+    loadPage(pages, pagesize, forum);
+    pages++;
+})
 
 const topic_submit = document.querySelector("#topic-submit");
 
--- js/func.js
+++ js/func.js
@@ -1,7 +1,58 @@
-// For future function we use
+import supabase from "./db.js";
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+async function createTopic(topic, forum) {
+    console.log("creating topic");
+    const {title, content, author_id, id} = topic;
+
+    const div = document.createElement("div");
+    forum.appendChild(div);
+    div.classList.add("topico");
+
+    const topic_title = document.createElement("h1");
+    topic_title.textContent = title;
+    div.appendChild(topic_title);
+
+    const topic_text = document.createElement("p");
+    topic_text.textContent = content;
+    div.appendChild(topic_text);
+
+    const topic_author = document.createElement("p");
+    const {data: author} = await supabase.from("profiles").select("*").eq("id", author_id).single();
+    topic_author.textContent = "Written by: " + author.username;
+    topic_author.classList.add("text-right");
+    div.appendChild(topic_author);
+    /* Dentre outras */
+
+    console.log(id);
+    div.addEventListener("click", () => {
+        let url = `./topic?id=${id}`;
+        window.location.href = url;
+        console.log(url);
+    });
 }
 
-export default sleep;
+
+async function loadPage(page, pagesize, forum) {
+    console.log("loading page");
+    const from = page * pagesize;
+    const to = (page + 1) * pagesize - 1;
+
+    const { data, error } = await supabase
+        .from("topics")
+        .select("*")
+        .order("created_at", { ascending: false })
+        .range(from, to);
+
+    console.log(data);
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    for (const topic of data) {
+        createTopic(topic, forum);
+    }
+}
+
+export { loadPage, createTopic };
